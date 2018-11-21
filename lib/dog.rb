e05fed3fc315213b5e5bfe3c2454e1d4bb68b575
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-21 09:41:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+  def name=(name)
+    @name = name
+  end
+  def name
+    "#{@name}".strip
+  end
+  def bread=(bread)
+    @bread = bread
+  end
+  def bread
+    "#{@bread}".strip
+  end
 end
